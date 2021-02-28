f003b9d8ef397b7f82ec06c827a94cf4a668d5fa
Moreno Gentili <brightsoul@tiscali.it>
2021-02-28 16:52:16 +0100

    Fix script

--- Program.cs
+++ Program.cs
@@ -15,14 +15,24 @@ namespace PuppeteerDemo
                 IgnoreHTTPSErrors = true,
                 Devtools = true
             });
+            
             using var page = await browser.NewPageAsync();
+            await Task.Delay(8000);
+            await page.GoToAsync("https://localhost:5001/Identity/Account/Login");
+            await page.TypeAsync("#Input_Email", "severino.padovano@example.com");
+            await page.TypeAsync("#Input_Password", "Teacher1!");
+            await Task.Delay(3000);
+            var navigationTask = page.WaitForNavigationAsync();
+            await page.ClickAsync(".container button[type=submit]");
+            await navigationTask;
+
             for (var i = 1; i < 32; i++)
             {
                 await page.GoToAsync($"https://localhost:5001/Contact?id={i}");
-                await page.TypeAsync("form textarea", "Lorem ipsum dolor sit amet");
+                await page.TypeAsync("#Question", "Lorem ipsum dolor sit amet");
                 await Task.Delay(1000);
-                var navigationTask = page.WaitForNavigationAsync();
-                await page.ClickAsync("form button");
+                navigationTask = page.WaitForNavigationAsync();
+                await page.ClickAsync(".container button[type=submit]");
                 await navigationTask;
                 await Task.Delay(1000);
             }
